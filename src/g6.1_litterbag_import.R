df578aa6e2adbcc1b40af47a3496ff7da0f9549e
yaya <tang.yaya@gmail.com>
2020-04-20 10:26:14 +0300

    redistribute code for litterbag data

--- src/g6.1_litterbag_import.R
+++ src/g6.1_litterbag_import.R
@@ -26,19 +26,6 @@ lit0_moist <- get_dry_lit() #[[1]] is the mean, [[2]] is the se
 lit_samps <- read_csv(here::here('data/lit_samplings.csv')) %>% 
     rename(samp_num = sampling)
 
-
-#==== create tibble for all litterbag sampling data
-# lit_moist <- tibble(sampling = integer(),
-#                     samp_desc = character())
-# 
-# # lit_moist <- tibble(moist_mean = double(),
-# #                     moist_se = double())
-# # assign values for pre-launch
-# lit_moist[1, 1] <- 0
-# lit_moist[1, 2] <- "pre-launch"
-
-
-
 # import cage properties and initial bag masses from July 2019
 cage_treatments <- read_csv(here::here('results/g1_updated_cages_wk9.csv')) %>% 
     select(-predatorID, -block, -position)
@@ -47,7 +34,7 @@ bag_prep_raw <- read_csv(here::here('data/litterbag_DATA_2019.07.11.csv'))
 colnames(bag_prep_raw) <- c('cage', 'dir', 'start_lit_wet', 'start_lit_bag')
 bag_prep <- bag_prep_raw %>% 
     filter(cage!=3 & cage!=26) %>% 
-    filter(!(cage==29 & dir=="E")) %>%
+    filter(!(cage==29 & dir=="E")) %>% # strange mass-gaining outlier in samp 1
     mutate(start_lit_dry = start_lit_wet - (start_lit_wet*lit0_moist[[1]]),
            m_bag = start_lit_bag - start_lit_wet) %>% 
     left_join(cage_treatments, by= 'cage') %>% 
@@ -71,14 +58,20 @@ bag_samp2 <- read_csv(here::here('data/litterbag_sampling2_E_DATA.csv'))
 colnames(bag_samp2) <- c('cage', 'dir', 'bag_wet', 'bag_dry')
 bag_samp2 <- mutate(bag_samp2, samp_num = 2)
 
+#==== import sampling 3 data
+bag_samp3 <- read_csv(here::here('data/litterbag_sampling3_W_S_DATAv2.csv'))
+colnames(bag_samp3) <- c('cage', 'dir', 'bag_wet', 'bag_dry')
+bag_samp3 <- mutate(bag_samp3, samp_num = 3)
+
+
 # merge sampling1 data with pre-launch data
-bag_data <- bind_rows(bag_samp0, bag_samp1, bag_samp2) %>% 
+bag_data <- bind_rows(bag_samp0, bag_samp1, bag_samp2, bag_samp3) %>% 
     left_join(bag_prep, by=c('cage', 'dir')) %>% 
     group_by(samp_num) %>% 
     mutate(lit_wet_m = bag_wet - m_bag, # for the wet mass of litter
            wet_diff = bag_wet - start_lit_bag, # for wet mass diff from the tstart
            dry_lit = bag_dry - m_bag, # dry mass of litter at this sampling
-           dry_diff = start_lit_dry - bag_dry - m_bag, # difference in dry litter mass from start
+           dry_diff_from_start = bag_dry - start_lit_dry - m_bag, # difference in dry litter mass from start
            moist = bag_wet - bag_dry,
            moist_percent = moist/lit_wet_m*100, # what is this a percentage of?
            bag_sampID = paste0(cage, dir, samp_num)) %>% 
@@ -87,7 +80,7 @@ bag_data <- bind_rows(bag_samp0, bag_samp1, bag_samp2) %>%
 # calculate mean moisture percentage and add to each 
 
 mean_moists <- bag_data %>% 
-    group_by(samp_num) %>% 
+    group_by(samp_num) %>%  #, dir) %>% 
     summarize(samp_moist.perc_mean = mean(moist_percent, na.rm=TRUE),
               samp_moist.perc_se = se(moist_percent))
 
@@ -98,23 +91,36 @@ lit_moist[1,]$samp_moist.perc_se <- lit0_moist[[2]] * 100
 
 
 # calculate the average moisture and infer dry masses
-bag_data2 <- bag_data %>% 
+bag_data_all <- bag_data %>% 
     left_join(lit_moist, by="samp_num") %>% 
     rename(moist_mean.perc = samp_moist.perc_mean,
            moist_se.perc = samp_moist.perc_se)  %>% 
-    mutate(
-        dry_infer = lit_wet_m - (moist_mean.perc/100*lit_wet_m),
-        bag_sampID = paste0(as.character(cage), dir, as.character(samp_num))) %>% 
+    mutate(dry_infer = lit_wet_m - (moist_mean.perc/100*lit_wet_m),
+           bag_sampID = paste0(as.character(cage), dir)) %>% #, as.character(samp_num))) %>% 
     select(bag_sampID, everything())
 
-bag_data3 <- bag_data2 %>% 
+bag_data_infer <- bag_data_all %>% 
     group_by(treatment, samp_num) %>% 
     summarize(mean.infer_trt = mean(dry_infer),
               se.infer_trt = se(dry_infer)) %>% 
     na.omit()
 
-bag_data_real <- bag_data2 %>% 
-    na.omit() %>% 
+#this is summarized by TREATMENT and TIME
+bag_data_real <- bag_data_all %>% 
+    na.omit() %>% # to get rid of inferred data
     group_by(treatment, samp_num) %>% 
     summarize(mean.true_trt = mean(dry_lit),
               se.true_trt = se(dry_lit))
+qqnorm(bag_data_real$mean.true_trt) #not sure this means ANYTHING, esp since 
+
+
+hist(data = bag_data_real, x = treatment, y = mean.true_trt)
+
+# check qqplots of real data, raw + unsummarized
+bag_data_msrd <- bag_data_all %>% 
+    select(-dry_infer) %>% 
+    na.omit() %>% 
+    filter(dry_diff_from_start != 0)
+
+qqnorm(bag_data_msrd$dry_diff_from_start)
+#qqline(bag_data_real$dry_diff_from_start) #, col = "steelblue", lwd = 2) #this line does nothing
--- src/g6.2_litterbag_viz.R
+++ src/g6.2_litterbag_viz.R
@@ -1,19 +1,158 @@
+library(plotly)
+
+# graph all litter dry masses over samplings
+all_bags <- ggplot(filter(bag_data_all, dir == 'W'),
+                   aes(x = samp_num, y = dry_infer, group = treatment)) +
+    # geom_line(aes(color = bag_sampID )) +
+    geom_jitter(aes(color = treatment), width = 0.2)
+ggplotly(all_bags)
+
+indiv_real <- ggplot(bag_data_all,
+                     aes(x = samp_num, y = dry_lit, group = treatment)) +
+    # geom_jitter(aes(color = dir))
+    geom_jitter(aes(color = treatment), width = 0.2)
+ggplotly(indiv_real)
+
+sw_compar <- bag_data_all %>%
+    filter(samp_num==3,
+           dir == 'S' | dir == 'W')
+# s_compar <- bag_data_all[which(bag_data_all$samp_num == 3 & bag_data_all$dir == 'S'),]
+# w_compar <- bag_data_all[which(bag_data_all$samp_num == 3 & bag_data_all$dir == 'W'),]
+s_compar <- filter(bag_data_all, samp_num == 3 & dir == 'S')
+w_compar <- filter(bag_data_all, samp_num == 3 & dir == 'W')
+
+t.test(filter(bag_data_all, dir == 'S' & samp_num == 0)$dry_lit, 
+       filter(bag_data_all, dir == 'S' & samp_num == 3)$dry_lit)
+    
+
+# t test comparing south from west
+t.test(s_compar$dry_lit, w_compar$dry_lit, paired = TRUE)
+
+# difference in dry litter mass
+t.test(s_compar$dry_diff_from_start, w_compar$dry_diff_from_start, paired = TRUE)
+
+sampW_aov <- aov(dry_diff_from_start ~ treatment, data = filter(bag_data_all, dir == 'W'))
+summary(sampW_aov)
+
+
+
+#ANOVA for the effect of litterbag dir on inferred dry mass @ T1
+samp1_aov <- aov(dry_infer ~ dir, data = filter(bag_data_all, samp_num == 1))
+summary(samp1_aov)
+
+
+# ANOVA for the effect of litterbag direction on inferred dry litter mass
+samp2_aov <- aov(dry_infer ~ dir, data = filter(bag_data_all, samp_num == 2))
+summary(samp2_aov)
+
+# ANOVA for the effect of litterbag direction on true  dry litter mass
+samp3_aov <- aov(dry_lit ~ dir, data = sw_compar)
+summary(samp3_aov)
+
+samp3_aov_trt <- aov(dry_lit ~ treatment, data = sw_compar)
+summary(samp3_aov_trt)
+
+
+some_aov <- aov(dry_infer ~ dir + treatment, data = filter(bag_data_all, samp_num == 2))
+# sw_compar)
+summary(some_aov)
+
+all_aov <- aov(dry_infer ~ treatment + samp_num, data = bag_data_all)
+summary(all_aov)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+indiv_real_s3 <- ggplot(sw_compar,
+                        aes(x = samp_num, y = dry_lit)) +
+    geom_point(aes(color = dir), size = 2)
+indiv_real_s3
+
+
 # inferred dry litter bag diffs over time
-ggplot(bag_data3, aes(x=samp_num, y=mean.infer_trt, group=treatment))+
+inferred <- ggplot(bag_data_infer, aes(x=samp_num, y=mean.infer_trt))+
     geom_line(aes(color=treatment)) +
     geom_point(aes(color=treatment))+
-    geom_errorbar(aes(ymin = mean.infer_trt - se.infer_trt, ymax = mean.infer_trt + se.infer_trt,
+    geom_errorbar(aes(ymin = mean.infer_trt - se.infer_trt, 
+                      ymax = mean.infer_trt + se.infer_trt,
                       color = treatment), width = 0.05)
+ggplotly(inferred) # though the inferred values shouldn't exist for samp 3...
 
-# REAL dry litter bag diffs over time
-ggplot(bag_data_real, aes(x=samp_num, y=mean.true_trt, group=treatment))+
+# REAL dry litter bag diffs over time, but AVERAGED for each treatment/sampling
+mean_real <- ggplot(bag_data_real, aes(x=samp_num, y=mean.true_trt))+
     geom_line(aes(color=treatment)) +
     geom_point(aes(color=treatment))+
-    geom_errorbar(aes(ymin = mean.true_trt - se.true_trt, ymax = mean.true_trt + se.true_trt,
+    geom_errorbar(aes(ymin = mean.true_trt - se.true_trt, 
+                      ymax = mean.true_trt + se.true_trt,
                       color = treatment), width = 0.05)
+ggplotly(mean_real)
+
+# =====
+# boxplots by treatment for the third sampling
+samp3_by.bag <- ggboxplot(filter(bag_data_all, samp_num==3), 
+                          x = 'samp_num', 
+                          y = 'dry_lit',
+                          color = 'treatment', 
+                          order = c('control', 'ghop', 'mantid', 'spider'),
+                          title = 'litterbag dry masses', 
+                          xlab = 'sampling number',
+                          ylab = 'dry mass') +
+    theme(legend.position='none')
+samp3_by.bag
+
+samp3_aov <- aov(dry_lit ~ treatment, data = filter(bag_data_all, samp_num==3))
+summary(samp3_aov)
+
+
+#====STATS FOR SAMPLING 1=====
+# =====summary stats=====
+# means by direction 
+final_by.dir <- group_by(bag_data_all, dir, samp_num)
+final_by.dir_summ <- summarise(final_by.dir, mean = mean(dry_lit), se = se(dry_lit)) %>% 
+    arrange(samp_num, dir)
+final_by.dir_summ
+
+
+final_by.trt <- group_by(bag_data_all, treatment, dir, samp_num)
+final_by.trt_summ <- summarise(final_by.trt, 
+                               mean = mean(dry_lit), 
+                               se = se(dry_lit)) %>% 
+    na.omit()
+final_by.trt_summ
+
+# =====ANOVAs=====
+# ANOVA between wet bags
+samp1_aov <- aov(samp1_wet_diff ~ treatment, data = bag_1)
+summary(samp1_aov)
+
+# ANOVA between bag direction
+samp1_aov_dir <- aov(samp1_wet_diff ~ dir, data = bag_1)
+summary(samp1_aov_dir)
+
+# ANOVA between dry bag treatments
+samp1_aov_dry <- aov(samp1_dry_diff ~ treatment, data = bag_north)
+summary(samp1_aov_dry)
+
+samp1_ttest_start <- t.test(bag_north$samp1_dry_lit, bag_north$start_lit_dry, paired=TRUE)
+samp1_ttest_start
 
 
 
+
+# =====================================================
+# ================== code below not functioning
 # boxplots for wet masses of all treatments and directions
 ggboxplot(bag_1, x = 'treatment', y = 'samp1_wet_diff',
           color = 'treatment', 
